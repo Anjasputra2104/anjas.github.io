0349d51dae6b6fbf2e42133b7e906ea367b3a370
anjasputra2104 <anjas.2104@students.amikom.ac.id>
2022-09-24 19:13:34 +0700

    portofolio

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ['*.{html,js}'],
+    theme: {
+        // container: {
+        //     center: true,
+        //     padding: '7rem',
+        // },
+        // colors: {
+        //     body: '#030E21',
+        //     white: '#FFFFFF',
+        //     primary: '#262E2A',
+        //     secondary: '#868490',
+        // },
+        extend: {},
+    },
+    plugins: [],
+};
